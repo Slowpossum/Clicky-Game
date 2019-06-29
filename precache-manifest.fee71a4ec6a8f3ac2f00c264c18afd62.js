6ab8db07d6b611b7b469a414576f8edabef08599
Slowpossum <tsmithva@verizon.net>
2019-06-29 09:58:02 -0400

    Updates

---
+++ precache-manifest.fee71a4ec6a8f3ac2f00c264c18afd62.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6e54cf76851d37287df7e7ef723143a8",
+    "url": "/Clicky-Game/index.html"
+  },
+  {
+    "revision": "0b2a3e7df2d989c0dd7e",
+    "url": "/Clicky-Game/static/css/main.45aa445a.chunk.css"
+  },
+  {
+    "revision": "45124f853ba07aa965c0",
+    "url": "/Clicky-Game/static/js/2.6efc73d3.chunk.js"
+  },
+  {
+    "revision": "0b2a3e7df2d989c0dd7e",
+    "url": "/Clicky-Game/static/js/main.1365e5a1.chunk.js"
+  },
+  {
+    "revision": "fab82da4d9420c34bc57",
+    "url": "/Clicky-Game/static/js/runtime~main.012bace8.js"
+  },
+  {
+    "revision": "f66c1ef50f54766c9627bad94e8fc94d",
+    "url": "/Clicky-Game/static/media/Diamond-Sunset.f66c1ef5.svg"
+  }
+]);
